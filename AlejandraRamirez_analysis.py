0a97186634b7f7769d9e7549fe7d93b63db47568
AlejandraRamirez <ma.ramirez@uniandes.edu.co>
2018-03-20 16:46:20 -0500

    Avance analysis.py

---
+++ AlejandraRamirez_analysis.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.optimize as sc
+
+#FUncion que retorna la distribucion normal de acuerdo a su formula
+#con promedio mean y desviacion estandar sigma
+
+def normal_dist(x,mean,sigma):
+    a = np.sqrt(1/float(2*np.pi*(sigma**2)))
+    c = (x-mean)**2
+    b = np.exp(-1*(c)/float(sigma**2))
+    sol = a*b
+
+#Funcion que carga array de promedios
+#y genera histograma a partir de los datos cargados
+
+def get_fit(filename):
+    #Carga los datos
+    data = np.genfromtxt(filename, usecols = 0)
+    #Retorna frecuencias y limites de hist
+    freqs,bins = np.histogram(data, normed = True)
+    
+    #Optimizar curva
+    #frecuencias
+    y = freqs
+    #bins
+    x = 0.5*(bins[:-1]+bins[1:]) 
+    #funcion de optimizacion
+    popt, pcov = sc.curve_fit(normal_dist,x,y)
+    mean = popt[0]
+    sigma = popt[1]
+    xx = linspace(np.min(data),np.max(data),100)
+    yy = normal_dist(xx,mean,sigma)
+
+    plt.plot(xx,yy, label = "ajuste normal")
+    plt.hist(data)
+    plt.save(filename+str(".png"))
+    plt.clf()
+
+get_fit("sample_1_10.txt")
+    
+    
+
+
